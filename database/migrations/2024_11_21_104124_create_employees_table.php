07ab8a1652fd431790825113a883875976fc0fc1
neuoavs <tungntt.23itb@vku.udn.vn>
2024-12-24 00:23:55 +0700

    change profile item

--- database/migrations/2024_11_21_104124_create_employees_table.php
+++ database/migrations/2024_11_21_104124_create_employees_table.php
@@ -17,14 +17,14 @@ return new class extends Migration
             $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade')->onUpdate('cascade');
             $table->string('password');
             $table->string('full_name', 40);
-            $table->string('nick_name', 20);
+            $table->string('nickname', 20);
             $table->date('birthday');
             $table->string('citizen_identification', 12);
             $table->string('phone_number', 11);
             $table->integer('salary_coefficient');
 
             //Rằng buộc
-            $table->unique(['nick_name', 'citizen_identification', 'phone_number']);
+            $table->unique(['nickname', 'citizen_identification', 'phone_number']);
         });
     }
 
