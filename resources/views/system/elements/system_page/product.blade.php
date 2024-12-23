b9d0b8b9dd22a20a639970be740f85d3c8c93f23
neuoavs <tungntt.23itb@vku.udn.vn>
2024-12-23 19:14:47 +0700

    change style

--- resources/views/system/elements/system_page/product.blade.php
+++ resources/views/system/elements/system_page/product.blade.php
@@ -1,6 +1,6 @@
 @if (Session::get('position') === 'Boss')
     <li class="submenu">
-        <a href="javascript:void(0);"><img src="{{asset('resources/assets/system/img/icons/product.svg')}}" alt="img"><span> Product</span> <span class="menu-arrow"></span></a>
+        <a href="javascript:void(0);"><img src="{{asset('resources/assets/system/img/icons/prd.svg')}}" alt="img"><span> Product</span> <span class="menu-arrow"></span></a>
         <ul>
             <li><a href="{{URL::to('/product-list')}}">Product List</a></li>
             <li><a href="{{URL::to('/product-add')}}">Add Product</a></li>
