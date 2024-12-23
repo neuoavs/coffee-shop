21c61c13425db62b3c2ef4d309bb2a110e460248
neuoavs <tungntt.23itb@vku.udn.vn>
2024-12-23 19:27:40 +0700

    change style

--- resources/views/system/elements/system_page/profile.blade.php
+++ resources/views/system/elements/system_page/profile.blade.php
@@ -1,5 +1,5 @@
 <li class="submenu">
-    <a href="javascript:void(0);"><img src="{{asset('resources/assets/system/img/icons/.svg')}}" alt="img"><span> Profile</span> <span class="menu-arrow"></span></a>
+    <a href="javascript:void(0);"><img src="{{asset('resources/assets/system/img/icons/prf.svg')}}" alt="img"><span> Profile</span> <span class="menu-arrow"></span></a>
     <ul>
         <li><a href="javascript:void(0);">Profile</a></li>
         <li><a href="{{URL::to('/logout')}}">Logout</a></li>
